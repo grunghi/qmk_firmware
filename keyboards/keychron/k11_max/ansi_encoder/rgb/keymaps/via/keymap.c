6cd70dcfc4fe49ebf21c8f1e7c354fee00784dea
grunghi <grunghi@gmail.com>
2024-08-15 18:00:30 +0200

    feat: add strings layer

--- keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/via/keymap.c
@@ -16,12 +16,62 @@
 
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
+#include "secrets.h"
 
 enum layers{
 	MAC_BASE,
 	WIN_BASE,
 	FN1,
 	FN2,
+	FN3,
+};
+
+enum {
+    TD_MAIL,
+    TD_TEL,
+    TD_ADR,
+    TD_NAME,
+};
+
+void send_email(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        SEND_STRING(PRIVATE_EMAIL);
+    } else if (state->count == 2) {
+        SEND_STRING(WORK_EMAIL);
+    }
+}
+
+void send_tel(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        SEND_STRING(TEL_WITH_PREFIX);
+    } else if (state->count == 2) {
+        SEND_STRING(TEL);
+    }
+}
+
+void send_address(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {    
+        SEND_STRING(ADDRESS);
+    } else if (state->count == 2) {    
+        SEND_STRING(OFFICE);
+    }
+}
+
+void send_name(qk_tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {    
+        SEND_STRING(FULL_NAME);
+    } else if (state->count == 2) {    
+        SEND_STRING(FIRST_NAME);
+    } else if (state->count == 3) {    
+        SEND_STRING(LAST_NAME);
+    }
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_MAIL] = ACTION_TAP_DANCE_FN(send_email),
+    [TD_TEL] = ACTION_TAP_DANCE_FN(send_tel),
+    [TD_ADR] = ACTION_TAP_DANCE_FN(send_address),
+    [TD_NAME] = ACTION_TAP_DANCE_FN(send_name),
 };
 
 // Left-hand home row mods
@@ -55,14 +105,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
         KC_CAPS, KC_A,	   KC_S,	 KC_D,	  KC_F,    KC_G,              KC_H,    KC_J,	KC_K,	 KC_L,	   KC_SCLN,  KC_QUOT,  KC_ENT,           KC_HOME,
         KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,	  KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LOPTN, KC_LCMMD,          KC_SPC,           MO(FN1),  MO(FN2),         KC_SPC,             KC_RCMMD,           KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LOPTN, KC_LCMMD,          LT(FN3, KC_SPC),  MO(FN1),  MO(FN2),         KC_SPC,             KC_RCMMD,           KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_69_ansi(
         KC_ESC,  KC_1,	   KC_2,	 KC_3,	  KC_4,    KC_5,	KC_6,	  KC_7,    KC_8,	KC_9,	 KC_0,	   KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
         KC_CAPS, GUI_A,    ALT_S,	 CTL_D,	  SFT_F,   KC_G,              KC_H,    SFT_J,	CTL_K,	 ALT_L,	   GUI_SCLN, KC_QUOT,  KC_ENT,           KC_HOME,
         KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,	  KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LWIN,  KC_LALT,           KC_SPC,           MO(FN1),  MO(FN2),          KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LWIN,  KC_LALT,           LT(FN3, KC_SPC),  MO(FN1),  MO(FN2),          KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
         
     [FN1] = LAYOUT_69_ansi(
         KC_TILD, KC_F1,    KC_F2,	 KC_F3,   KC_F4,   KC_F5,	KC_F6,	  KC_F7,   KC_F8,	KC_F9,	 KC_F10,   KC_F11,	 KC_F12,   _______,          _______,
@@ -76,7 +126,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, BT_HST1,  BT_HST2,  BT_HST3, P2P4G,   BAT_LVL, _______,  _______, _______, _______, _______,  _______,	 _______,  _______,          KC_INS,
         RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI,           _______, _______, _______, _______,  _______,  _______,  _______,          KC_END,
         _______,           RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD,  _______, NK_TOGG, _______, _______,  _______,  _______,  _______, KC_PGUP,
-        _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, KC_PGDN, _______)
+        _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, KC_PGDN, _______),
+        
+    [FN3] = LAYOUT_69_ansi(
+        _______, _______, _______,    _______,     _______,     _______, _______, _______, _______,    _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______,    _______,     _______,     _______, _______, _______, _______,    _______, _______, _______, _______, _______,          _______,
+        _______, _______, TD(TD_ADR), TD(TD_NAME), TD(TD_MAIL), _______,          _______, TD(TD_TEL), _______, _______, _______, _______, _______,          _______,
+        _______,          _______,    _______,     _______,     _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,                 _______,              _______, _______,             _______,          _______,          _______, _______, _______)
+        
+    // [EMPTY] = LAYOUT_69_ansi(
+    //     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+    //     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+    //     _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______,          _______,
+    //     _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    //     _______, _______, _______,          _______,          _______, _______,          _______,          _______,          _______, _______, _______)
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -89,7 +153,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #endif // ENCODER_MAP_ENABLE
 
 // clang-format on
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) { 
     if (!process_record_keychron_common(keycode, record)) {
         return false;
     }

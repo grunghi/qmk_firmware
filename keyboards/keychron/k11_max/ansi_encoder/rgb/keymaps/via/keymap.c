8090a286dd21256792735d14a4038de01aeb11d7
grunghi <grunghi@gmail.com>
2024-08-21 12:14:36 +0200

    feat: add layer lock key

--- keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/via/keymap.c
@@ -18,6 +18,7 @@
 #include "keychron_common.h"
 #include "secrets.h"
 #include "config.h"
+#include "features/layer_lock.h"
 
 enum layers{
 	MAC_BASE,
@@ -34,6 +35,10 @@ enum {
     TD_NAME,
     TD_CRD,
     TD_LOG,
+};
+
+enum custom_keycodes {
+  LLOCK = SAFE_RANGE,
 };
 
 void send_email(tap_dance_state_t *state, void *user_data) {
@@ -138,14 +143,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,  KC_PGUP, KC_UP,   _______, _______, KC_NUM,  KC_P7,   KC_P8,   KC_P9,   KC_SLSH,  KC_ASTR,  KC_PMNS,  _______,          _______,
         _______, KC_HOME,  KC_LEFT, KC_DOWN, KC_RGHT, KC_END,           KC_BSPC, SFT_4,   CTL_5,   ALT_6,    GUI_PLS,  KC_DEL,   _______,          _______,
         _______,           KC_PGDN, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,    KC_P3,    KC_PEQL,  _______, _______,
-        _______, _______,  _______,          _______,          _______, _______,          KC_P0,             KC_PDOT,            _______, _______, _______),
+        _______, _______,  _______,          _______,          LLOCK,   _______,          KC_P0,             KC_PDOT,            _______, _______, _______),
 
     [FN1] = LAYOUT_69_ansi(
         KC_GRV,  KC_BRID,  KC_BRIU,  KC_TASK, KC_FILE, RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          RGB_TOG,
         _______, BT_HST1,  BT_HST2,  BT_HST3, P2P4G,   BAT_LVL, _______,  _______, _______, _______, _______,  _______,	 _______,  _______,          KC_INS,
         RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI,           _______, _______, _______, _______,  _______,  _______,  _______,          KC_END,
         _______,           RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD,  _______, NK_TOGG, _______, _______,  _______,  _______,  _______, KC_PGUP,
-        _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, KC_PGDN, _______),
+        _______, _______,  _______,           _______,          _______,  LLOCK,            _______,           _______,            _______, KC_PGDN, _______),
         
     [FN3] = LAYOUT_69_ansi(
         _______, _______, _______,    _______,     _______,       _______, _______, _______, _______,    _______,    _______, _______, _______, _______,          _______,
@@ -173,10 +178,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #endif // ENCODER_MAP_ENABLE
 
 // clang-format on
-bool process_record_user(uint16_t keycode, keyrecord_t *record) { 
-    if (!process_record_keychron_common(keycode, record)) {
-        return false;
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!process_layer_lock(keycode, record, LLOCK)) { 
+        return false; 
     }
+    
+    if (!process_record_keychron_common(keycode, record)) { 
+        return false; 
+    }
+    
     return true;
 }
 
@@ -189,8 +199,8 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
                 uint8_t index = g_led_config.matrix_co[row][col];
 
                 if (index >= led_min && index < led_max && index != NO_LED &&
-                keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-                    rgb_matrix_set_color(index, RGB_RED);
+                    keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                    rgb_matrix_set_color(index, RGB_TEAL);
                 }
             }
         }

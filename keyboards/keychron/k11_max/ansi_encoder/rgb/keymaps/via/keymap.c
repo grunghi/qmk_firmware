97991058fda7f65ca550c96b961e73074341af6d
grunghi <grunghi@gmail.com>
2024-09-05 10:22:39 +0200

    feat: add right shift enter modtap

--- keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/via/keymap.c
@@ -35,10 +35,13 @@ enum {
     TD_NAME,
     TD_CRD,
     TD_LOG,
+    TD_J,
 };
 
 enum custom_keycodes {
   LLOCK = SAFE_RANGE,
+  CTRL_GRV = SAFE_RANGE,
+  CTRL_SHIFT_GRV,
 };
 
 void send_email(tap_dance_state_t *state, void *user_data) {
@@ -88,11 +91,29 @@ void send_test_card(tap_dance_state_t *state, void *user_data) {
 void send_console_log(tap_dance_state_t *state, void *user_data) {
     if (state->count == 1) {    
         SEND_STRING("console.log()");
-        // tap_code(KC_LEFT);
+        tap_code(KC_LEFT);
     } else if (state->count == 2) {
         SEND_STRING("console.error()");
-        // tap_code(KC_LEFT);
+        tap_code(KC_LEFT);
     }
+}
+
+void open_terminal(tap_dance_state_t *state, void *user_data) {
+    if (state->count == 1) {
+        register_code(KC_LCTL);
+        tap_code(KC_GRV);
+        unregister_code(KC_LCTL);
+    } else if (state->count == 2) {
+        register_code(KC_LCTL);
+        register_code(KC_LSFT);
+        tap_code(KC_GRV);
+        unregister_code(KC_LSFT);
+        unregister_code(KC_LCTL);
+    }
+}
+
+void dance_j_reset(tap_dance_state_t *state, void *user_data) {
+    // Nothing to reset
 }
 
 tap_dance_action_t tap_dance_actions[] = {
@@ -102,6 +123,7 @@ tap_dance_action_t tap_dance_actions[] = {
     [TD_NAME] = ACTION_TAP_DANCE_FN(send_name),
     [TD_CRD] = ACTION_TAP_DANCE_FN(send_test_card),
     [TD_LOG] = ACTION_TAP_DANCE_FN(send_console_log),
+    [TD_J] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, open_terminal, dance_j_reset)
 };
 
 // Left-hand home row mods
@@ -121,7 +143,10 @@ tap_dance_action_t tap_dance_actions[] = {
 #define CTL_5 RCTL_T(KC_P5)
 #define ALT_6 LALT_T(KC_P6)
 #define GUI_PLS RGUI_T(KC_PPLS)
-	
+
+// Enter / right shift mod
+#define RSFT_ENT RSFT_T(KC_ENT)
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_69_ansi(
@@ -129,26 +154,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
         KC_CAPS, KC_A,	   KC_S,	 KC_D,	  KC_F,    KC_G,              KC_H,    KC_J,	KC_K,	 KC_L,	   KC_SCLN,  KC_QUOT,  KC_ENT,           KC_HOME,
         KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,	  KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LOPTN, KC_LCMMD,          LT(FN3, KC_SPC),  MO(FN1),  MO(FN2),          KC_SPC,             KC_RCMMD,           KC_LEFT, KC_DOWN, KC_RGHT),
+        KC_LCTL, KC_LOPTN, KC_LCMMD,          LT(FN3, KC_SPC),  MO(FN1),  MO(FN2),          KC_SPC,            KC_RCMMD,           KC_LEFT, KC_DOWN, KC_RGHT),
 
     [WIN_BASE] = LAYOUT_69_ansi(
         KC_ESC,  KC_1,	   KC_2,	 KC_3,	  KC_4,    KC_5,	KC_6,	  KC_7,    KC_8,	KC_9,	 KC_0,	   KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
         KC_CAPS, GUI_A,    ALT_S,	 CTL_D,	  SFT_F,   KC_G,              KC_H,    SFT_J,	CTL_K,	 ALT_L,	   GUI_SCLN, KC_QUOT,  KC_ENT,           KC_HOME,
-        KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,	  KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  KC_RSFT, KC_UP,
+        KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,	  KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  RSFT_ENT, KC_UP,
         KC_LCTL, KC_LWIN,  KC_LALT,           LT(FN3, KC_SPC),  MO(FN1),  MO(FN2),          KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
         
     [FN2] = LAYOUT_69_ansi(
         KC_TILD, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,	  KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          _______,
         _______, _______,  KC_PGUP, KC_UP,   _______, _______, KC_NUM,  KC_P7,   KC_P8,   KC_P9,   KC_SLSH,  KC_ASTR,  KC_PMNS,  _______,          _______,
         _______, KC_HOME,  KC_LEFT, KC_DOWN, KC_RGHT, KC_END,           KC_BSPC, SFT_4,   CTL_5,   ALT_6,    GUI_PLS,  KC_DEL,   _______,          _______,
-        _______,           KC_PGDN, _______, _______, _______, _______, _______, _______, KC_P1,   KC_P2,    KC_P3,    KC_PEQL,  _______, _______,
+        _______,           KC_PGDN, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,    KC_PEQL,  _______,  _______, _______,
         _______, _______,  _______,          _______,          LLOCK,   _______,          KC_P0,             KC_PDOT,            _______, _______, _______),
 
     [FN1] = LAYOUT_69_ansi(
         KC_GRV,  KC_BRID,  KC_BRIU,  KC_TASK, KC_FILE, RGB_VAD, RGB_VAI,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,          RGB_TOG,
         _______, BT_HST1,  BT_HST2,  BT_HST3, P2P4G,   BAT_LVL, _______,  _______, _______, _______, _______,  _______,	 _______,  _______,          KC_INS,
-        RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI,           _______, _______, _______, _______,  _______,  _______,  _______,          KC_END,
+        RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI,           _______, TD(TD_J),_______, _______,  _______,  _______,  _______,          KC_END,
         _______,           RGB_RMOD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD,  _______, NK_TOGG, _______, _______,  _______,  _______,  _______, KC_PGUP,
         _______, _______,  _______,           _______,          _______,  LLOCK,            _______,           _______,            _______, KC_PGDN, _______),
         

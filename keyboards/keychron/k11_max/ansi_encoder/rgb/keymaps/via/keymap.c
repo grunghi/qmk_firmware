33885d0215f3907c5dea79ce2bedc2c513b70e22
grunghi <grunghi@gmail.com>
2024-09-10 16:14:48 +0200

    feat: revert rsft enter mod

--- keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/via/keymap.c
@@ -144,8 +144,6 @@ tap_dance_action_t tap_dance_actions[] = {
 #define ALT_6 LALT_T(KC_P6)
 #define GUI_PLS RGUI_T(KC_PPLS)
 
-// Enter / right shift mod
-#define RSFT_ENT RSFT_T(KC_ENT)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -160,7 +158,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_1,	   KC_2,	 KC_3,	  KC_4,    KC_5,	KC_6,	  KC_7,    KC_8,	KC_9,	 KC_0,	   KC_MINS,  KC_EQL,   KC_BSPC,          KC_MUTE,
         KC_TAB,  KC_Q,	   KC_W,	 KC_E,	  KC_R,    KC_T,	KC_Y,	  KC_U,    KC_I,	KC_O,	 KC_P,	   KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
         KC_CAPS, GUI_A,    ALT_S,	 CTL_D,	  SFT_F,   KC_G,              KC_H,    SFT_J,	CTL_K,	 ALT_L,	   GUI_SCLN, KC_QUOT,  KC_ENT,           KC_HOME,
-        KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,	  KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  RSFT_ENT, KC_UP,
+        KC_LSFT,           KC_Z,	 KC_X,	  KC_C,    KC_V,	KC_B,	  KC_B,    KC_N,	KC_M,	 KC_COMM,  KC_DOT,	 KC_SLSH,  KC_RSFT, KC_UP,
         KC_LCTL, KC_LWIN,  KC_LALT,           LT(FN3, KC_SPC),  MO(FN1),  MO(FN2),          KC_SPC,            KC_RALT,            KC_LEFT, KC_DOWN, KC_RGHT),
         
     [FN2] = LAYOUT_69_ansi(

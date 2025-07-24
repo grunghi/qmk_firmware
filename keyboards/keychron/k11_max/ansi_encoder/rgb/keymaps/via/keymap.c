d64b6a211768baf2ed473a8b5859b43f56eaba01
grunghi <grunghi@gmail.com>
2025-07-24 15:35:34 +0200

    feat: push top numpad row one column to the right

--- keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k11_max/ansi_encoder/rgb/keymaps/via/keymap.c
@@ -164,7 +164,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         
     [FN2] = LAYOUT_69_ansi(
         KC_TILD, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,	  KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          _______,
-        _______, _______,  KC_PGUP, KC_UP,   _______, _______, KC_NUM,  KC_P7,   KC_P8,   KC_P9,   KC_SLSH,  KC_ASTR,  KC_PMNS,  _______,          _______,
+        _______, _______,  KC_PGUP, KC_UP,   _______, _______, _______, KC_NUM,  KC_P7,   KC_P8,   KC_P9,    KC_SLSH,  KC_ASTR,  KC_PMNS,           _______,
         _______, KC_HOME,  KC_LEFT, KC_DOWN, KC_RGHT, KC_END,           KC_BSPC, SFT_4,   CTL_5,   ALT_6,    GUI_PLS,  KC_DEL,   _______,          _______,
         _______,           KC_PGDN, _______, _______, _______, _______, _______, KC_P1,   KC_P2,   KC_P3,    KC_PEQL,  _______,  _______, _______,
         _______, _______,  _______,          _______,          LLOCK,   _______,          KC_P0,             KC_PDOT,            _______, _______, _______),
